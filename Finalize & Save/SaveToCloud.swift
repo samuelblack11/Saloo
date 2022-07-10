bbb56d5b0e5a38ae29a47a7882cff4cae3cab453
Sam Black <samblack@Sams-iMac.local>
2022-07-10 09:36:24 -0400

    Synced eCards to iCloud Account (NSPersistentCloudKitContainer)

---
+++ Finalize & Save/SaveToCloud.swift
@@ -0,0 +1,8 @@
+//
+//  SaveToCloud.swift
+//  GreetMe-2
+//
+//  Created by Sam Black on 7/10/22.
+//
+
+import Foundation
--- GreetMe-2/Model/DataController.swift
+++ GreetMe-2/Model/DataController.swift
@@ -7,10 +7,11 @@
 
 import Foundation
 import CoreData
+import UIKit
 // Created using https://www.hackingwithswift.com/read/38/2/designing-a-core-data-model
 // https://www.hackingwithswift.com/read/38/3/adding-core-data-to-our-project-nspersistentcontainer
 // https://www.hackingwithswift.com/read/38/4/creating-an-nsmanagedobject-subclass-with-xcode
-class DataController {
+class DataController: UIResponder, UIApplicationDelegate {
     
     // Load the container once here, so it can be used elsewhere in the app
     // NSPersistentContainer allows for the following:
@@ -24,32 +25,48 @@ class DataController {
     
     // if the database exists, loadPersistentStores will load it. If does not exist, this line will create it
     
+
+    // ...
     static let shared = DataController()
-
     
-    let container: NSPersistentContainer
-    let backgroundContext:NSManagedObjectContext!
-
+    lazy var container: NSPersistentCloudKitContainer = {
+    
+        let container2 = NSPersistentCloudKitContainer(name: "GreetMe_2")
+        container2.loadPersistentStores(completionHandler: {
+            (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container2
+    }()
+    
     var viewContext:NSManagedObjectContext {
         return container.viewContext
     }
     
-    init() {
-        container = NSPersistentContainer(name: "GreetMe_2")
-        container.loadPersistentStores { storeDescription, error in
-            if let error = error {
-                print("Unresolved error \(error)")
-            }
-        }
-        backgroundContext = container.newBackgroundContext()
-    }
+    // ...
+}
+    
+    //https://developer.apple.com/documentation/coredata/mirroring_a_core_data_store_with_cloudkit/setting_up_core_data_with_cloudkit
+   // init() {
+        
+        //container = NSPersistentContainer(name: "GreetMe_2")
+       // container = NSPersistentCloudKitContainer(name: "GreetMe_2")
+       // container.loadPersistentStores { storeDescription, error in
+       // print("Store Description: \(storeDescription)")
+       //     if let error = error {
+        //        print("Unresolved error \(error)")
+        //    }
+       // }
+        //backgroundContext = container.newBackgroundContext()
+    //}
     
     // https://www.hackingwithswift.com/read/38/3/adding-core-data-to-our-project-nspersistentcontainer
-    func load() {
-        container.loadPersistentStores { storeDescription, error in
-            if let error = error {
-                print("Unresolved error \(error)")
-            }
-        }
-    }
-}
+    //func load() {
+     //   container.loadPersistentStores { storeDescription, error in
+      //      if let error = error {
+      //          print("Unresolved error \(error)")
+      //      }
+      //  }
+    //}
