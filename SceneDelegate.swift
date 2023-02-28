ebca2b75527723ca248f14a000b3dd1e7fe631a4
Sam Black <samblack@Sams-iMac.local>
2023-02-28 18:30:45 -0500

    Setup fetch share for testing on ckshare accept

--- SceneDelegate.swift
+++ SceneDelegate.swift
@@ -14,32 +14,33 @@ import SwiftUI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate, ObservableObject {
     var window: UIWindow?
     @State var coreCard: CoreCard?
+    @State var ckShare: CKShare?
     //var musicSubTimeToAddMusic: Bool = false
     //var musicSubType: MusicSubscriptionOptions = .Neither
     //@EnvironmentObject var appDelegate: AppDelegate
     //@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
+    
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         //self.scene(scene, openURLContexts: connectionOptions.urlContexts)
         // Create the SwiftUI view that provides the window contents.
-        //let contentView = MusicSearchView()
-        //print("called willConnectTo")
-
+        let contentView = EnlargeECardView(chosenCard: coreCard!, share: $ckShare, cardsForDisplay: [], whichBoxVal: .inbox)
+        print("called willConnectTo")
+        
         // Use a UIHostingController as window root view controller.
-       // if let windowScene = scene as? UIWindowScene {
-       //     let window = UIWindow(windowScene: windowScene)
-       //     window.rootViewController = UIHostingController(rootView: contentView)
-        //    self.window = window
-       //     window.makeKeyAndVisible()
-       //}
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
         
         
         
-       // let url = connectionOptions.urlContexts.first?.url
+        // let url = connectionOptions.urlContexts.first?.url
         //self.scene(scene, openURLContexts: url)
-
-      
+        
+        
     }
     
     /**
@@ -52,19 +53,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, ObservableObject {
         container.acceptShareInvitations(from: [cloudKitShareMetadata], into: sharedStore) { [self] (_, error) in
             if let error = error {print("\(#function): Failed to accept share invitations: \(error)")}
             else {
+                print("11111")
+                ckShare = cloudKitShareMetadata.share
                 self.fetchShare(cloudKitShareMetadata)
                 self.shareStatus(card: self.coreCard!)
-                let acceptedECardView = self.window?.rootViewController as! EnlargeECardView()
-                EnlargeECardView(chosenCard: coreCard!, share: cloudKitShareMetadata.share, cardsForDisplay: [], whichBoxVal: .inbox)
-                
-                
-                
-                
-                
                 
                 // add card to inbox
                 // display card via -> Inbox -> EnlargeECardView -> eCardView
-                print("------")
+                print("22222")
                 print(cloudKitShareMetadata.rootRecord?.object(forKey: "songName"))
                 //EnlargeECardView(chosenCard: , share: , cardsForDisplay: persistenceController., whichBoxVal: .inbox)
                 //if owner of card, whichBoxVal: .outbox. Else, .inbox
@@ -82,54 +78,50 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, ObservableObject {
     }
     
     func fetchShare(_ metadata: CKShare.Metadata) {
-        //var coreCard = CoreCard?
-        let operation = CKFetchRecordsOperation(
-            //recordIDs: [metadata.rootRecord?.recordID?.map{String($0)}])
-            recordIDs: [metadata.rootRecordID])
-        operation.perRecordResultBlock = { record, _, error in
-            if error != nil { print(error?.localizedDescription as Any)}
-
-            if record != nil {
-                
-                
-                
-                
+        let operation = CKFetchRecordsOperation(recordIDs: [metadata.hierarchicalRootRecordID!])
+        operation.perRecordResultBlock! = { recordID, recordResult in
+            switch recordResult {
+            case .success(let ref):
                 DispatchQueue.main.async() {
-                    //self.associatedRecord = record
-                    //self.cardName = record?.object(forKey: "cardName") as? String
-                    coreCard?.cardName = record?.object(forKey: "cardName") as! String
-                    self.coreCard?.occassion = record?.object(forKey: "occassion") as! String
-                    self.coreCard?.recipient = record?.object(forKey: "recipient") as! String
-                    self.coreCard?.sender = record?.object(forKey: "sender") as? String
-                    self.coreCard?.an1 = record?.object(forKey: "an1") as! String
-                    self.coreCard?.an2 = record?.object(forKey: "an2") as! String
-                    self.coreCard?.an2URL = record?.object(forKey: "an2URL") as! String
-                    self.coreCard?.an3 = record?.object(forKey: "an3") as! String
-                    self.coreCard?.an4 = record?.object(forKey: "an4") as! String
-                    self.coreCard?.collage = record?.object(forKey: "collage") as? Data
-                    self.coreCard?.coverImage = record?.object(forKey: "coverImage") as? Data
-                    self.coreCard?.date = record?.object(forKey: "date") as! Date
-                    self.coreCard?.font = record?.object(forKey: "font") as! String
-                    self.coreCard?.message = record?.object(forKey: "message") as! String
-                    self.coreCard?.songID = record?.object(forKey: "songID") as? String
-                    self.coreCard?.spotID = record?.object(forKey: "spotID") as? String
-                    self.coreCard?.songName = record?.object(forKey: "songName") as? String
-                    self.coreCard?.songArtistName = record?.object(forKey: "songArtistName") as? String
-                    self.coreCard?.songArtImageData = record?.object(forKey: "songArtImageData") as? Data
-                    self.coreCard?.songPreviewURL = record?.object(forKey: "songPreviewURL") as? String
-                    self.coreCard?.songDuration = record?.object(forKey: "songDuration") as? String
-                    self.coreCard?.inclMusic = record?.object(forKey: "inclMusic") as! Bool
-                    self.coreCard?.spotImageData = record?.object(forKey: "spotImageData") as? Data
-                    self.coreCard?.spotSongDuration = record?.object(forKey: "spotSongDuration") as? String
-                    self.coreCard?.spotPreviewURL = record?.object(forKey: "spotPreviewURL") as? String
+                    self.coreCard?.cardName = ref.object(forKey: "cardName") as! String
+                    self.coreCard?.occassion = ref.object(forKey: "occassion") as! String
+                    self.coreCard?.recipient = ref.object(forKey: "recipient") as! String
+                    self.coreCard?.sender = ref.object(forKey: "sender") as? String
+                    self.coreCard?.an1 = ref.object(forKey: "an1") as! String
+                    self.coreCard?.an2 = ref.object(forKey: "an2") as! String
+                    self.coreCard?.an2URL = ref.object(forKey: "an2URL") as! String
+                    self.coreCard?.an3 = ref.object(forKey: "an3") as! String
+                    self.coreCard?.an4 = ref.object(forKey: "an4") as! String
+                    self.coreCard?.collage = ref.object(forKey: "collage") as? Data
+                    self.coreCard?.coverImage = ref.object(forKey: "coverImage") as? Data
+                    self.coreCard?.date = ref.object(forKey: "date") as! Date
+                    self.coreCard?.font = ref.object(forKey: "font") as! String
+                    self.coreCard?.message = ref.object(forKey: "message") as! String
+                    self.coreCard?.songID = ref.object(forKey: "songID") as? String
+                    self.coreCard?.spotID = ref.object(forKey: "spotID") as? String
+                    self.coreCard?.songName = ref.object(forKey: "songName") as? String
+                    self.coreCard?.songArtistName = ref.object(forKey: "songArtistName") as? String
+                    self.coreCard?.songArtImageData = ref.object(forKey: "songArtImageData") as? Data
+                    self.coreCard?.songPreviewURL = ref.object(forKey: "songPreviewURL") as? String
+                    self.coreCard?.songDuration = ref.object(forKey: "songDuration") as? String
+                    self.coreCard?.inclMusic = ref.object(forKey: "inclMusic") as! Bool
+                    self.coreCard?.spotImageData = ref.object(forKey: "spotImageData") as? Data
+                    self.coreCard?.spotSongDuration = ref.object(forKey: "spotSongDuration") as? String
+                    self.coreCard?.spotPreviewURL = ref.object(forKey: "spotPreviewURL") as? String
                 }
+            case .failure:
+                print("Record Result Returned Error")
             }
         }
-
-        operation.fetchRecordsCompletionBlock = { _, error in
-            if error != nil { print(error?.localizedDescription)}
+        
+        operation.fetchRecordsResultBlock = { result in
+            switch result {
+                case .success:
+                    print("Disregard")
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            CKContainer.default().sharedCloudDatabase.add(operation)
         }
-        CKContainer.default().sharedCloudDatabase.add(operation)
     }
-
 }
